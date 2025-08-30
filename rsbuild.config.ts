aa64b811269cd8d6b123e2a55d5ccd5ef6c44e46
조하은 <jhe93@naver.com>
2025-08-30 15:39:55 +0900

    chore(#2): tanstack router 설치 및 테스트

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,26 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { TanStackRouterRspack } from '@tanstack/router-plugin/rspack';
 
 export default defineConfig({
+    source: {
+        entry: {
+            index: './src/index',
+        },
+    },
     plugins: [pluginReact()],
+    tools: {
+        rspack: {
+            plugins: [
+                TanStackRouterRspack({
+                    target: 'react',
+                    autoCodeSplitting: true,
+                    routesDirectory: './src/app/routes',
+                    generatedRouteTree: './src/routeTree.gen.ts',
+                    routeFileIgnorePrefix: '-',
+                    quoteStyle: 'single',
+                }),
+            ],
+        },
+    },
 });
---
+++ src/app/routes/index.tsx
@@ -0,0 +1,15 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "../../shared/ui/button/ui";
+
+export const Route = createFileRoute("/")({
+	component: Index,
+});
+
+function Index() {
+	return (
+		<div className="p-2">
+			<h3>Welcome Home!</h3>
+			<Button>겜구 테스트</Button>
+		</div>
+	);
+}
