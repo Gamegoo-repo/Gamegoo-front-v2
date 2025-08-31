422454fcb7614f40b0b61f0fbea00cb98928ae46
조하은 <jhe93@naver.com>
2025-08-31 11:49:09 +0900

    feat: 로고 버튼 컴포넌트 작성

---
+++ src/shared/ui/logo/LogoButton.tsx
@@ -0,0 +1,15 @@
+import { Link } from "@tanstack/react-router";
+import { Logo } from "./Logo";
+
+type LogoProps = {
+	className?: string;
+	size?: "sm" | "md" | "lg";
+};
+
+export function LogoButton({ className, size }: LogoProps) {
+	return (
+		<Link to="/">
+			<Logo className={className} size={size} />
+		</Link>
+	);
+}
