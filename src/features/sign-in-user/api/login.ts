038489dd3701d41284fbbbf5802e89e6a50ee19a
조하은 <jhe93@naver.com>
2025-09-07 16:35:36 +0900

    fix: login 코드 수정

--- src/features/sign-in-user/api/login.ts
+++ src/features/sign-in-user/api/login.ts
@@ -1,23 +1,26 @@
 import { encode } from "js-base64";
 
+
 export const login = () => {
-	const redirect = 'https://local.gamegoo.co.kr/riot/callback';
+	const frontend_redirect = 'https://gamegoo.co.kr/riot/callback';
 
 	const riotAuthUrl = import.meta.env.PUBLIC_RIOT_AUTH_URL;
-	const redirectUri = import.meta.env.PUBLIC_RIOT_REDIRECT_URI;
+	const SERVER_CALLBACK = import.meta.env.PUBLIC_RIOT_REDIRECT_URI;
 	const clientId = import.meta.env.PUBLIC_RIOT_CLIENT_ID;
 	const responseType = import.meta.env.PUBLIC_RIOT_RESPONSE_TYPE;
 	const scope = import.meta.env.PUBLIC_RIOT_SCOPE;
 
-	const encodedState = encode(JSON.stringify({ redirect }));
+	const state = {
+      frontend_redirect,
+    };
+
+	const encodedState = encode(JSON.stringify(state));
 
 	const authUrl = `${riotAuthUrl}?redirect_uri=${encodeURIComponent(
-		redirectUri,
+		SERVER_CALLBACK,
 	)}&client_id=${clientId}&response_type=${responseType}&scope=${scope}&state=${encodeURIComponent(
 		encodedState,
 	)}&prompt=login`;
-
-	console.log(authUrl);
 
 	window.location.href = authUrl;
 };
