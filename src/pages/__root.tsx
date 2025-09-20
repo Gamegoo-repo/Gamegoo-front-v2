e402ac38cf04cedeb45a2907c22e77601eac4192
조하은 <jhe93@naver.com>
2025-09-20 16:18:48 +0900

    remove(#41): TanStack Router 예제 제거

--- src/pages/__root.tsx
+++ src/pages/__root.tsx
@@ -1,28 +1,13 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { useChatNotifications } from "@/features/chat-notifications";
-import { FloatingChatButton } from "@/widgets/floating-chat-button";
 
 function RootLayout() {
 	useChatNotifications();
 
-	const handleChatButtonClick = () => {
-		console.log("Chat button clicked!");
-	};
-
 	return (
 		<>
-			<div className="p-2 flex gap-2">
-				<Link to="/" className="[&.active]:font-bold">
-					Home
-				</Link>
-				<Link to="/about" className="[&.active]:font-bold">
-					About
-				</Link>
-			</div>
-			<hr />
 			<Outlet />
-			<FloatingChatButton onClick={handleChatButtonClick} />
 			<TanStackRouterDevtools />
 		</>
 	);
