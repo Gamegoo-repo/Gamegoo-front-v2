0355b712bce48a22dc6d3f75b19c90cdc8e8cc21
Sungwook Kim <silenc3@naver.com>
2025-09-23 20:11:41 +0900

    chore(#15): socket provider & tanstack query provider 추가

--- src/pages/__root.tsx
+++ src/pages/__root.tsx
@@ -3,6 +3,10 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { useState } from "react";
 import { useChatDialogStore } from "@/features/chat-dialog-controller";
 import { useChatNotifications } from "@/features/chat-notifications";
+import {
+	GamegooSocketProvider,
+	TanstackQueryProvider,
+} from "@/shared/providers";
 import { ChatContent, ChatDialog, ChatTabs } from "@/widgets/chat-dialog";
 import { FloatingChatButton } from "@/widgets/floating-chat-button";
 
@@ -23,27 +27,30 @@ function RootLayout() {
 	const tabs = ["친구 목록", "채팅방"];
 
 	return (
-		<>
-			<div className="p-2 flex gap-2">
-				<Link to="/" className="[&.active]:font-bold">
-					Home
-				</Link>
-				<Link to="/about" className="[&.active]:font-bold">
-					About
-				</Link>
-			</div>
-			<hr />
-			<Outlet />
-			<FloatingChatButton onClick={handleChatButtonClick} />
-			<ChatDialog
-				isOpen={isOpen}
-				onClose={handleChatDialogClose}
-			>
-				<ChatTabs tabs={tabs} activeTab={activeTab} onTabClick={setActiveTab} />
-				<ChatContent activeTab={activeTab} />
-			</ChatDialog>
-			<TanStackRouterDevtools />
-		</>
+		<TanstackQueryProvider>
+			<GamegooSocketProvider>
+				<div className="p-2 flex gap-2">
+					<Link to="/" className="[&.active]:font-bold">
+						Home
+					</Link>
+					<Link to="/about" className="[&.active]:font-bold">
+						About
+					</Link>
+				</div>
+				<hr />
+				<Outlet />
+				<FloatingChatButton onClick={handleChatButtonClick} />
+				<ChatDialog isOpen={isOpen} onClose={handleChatDialogClose}>
+					<ChatTabs
+						tabs={tabs}
+						activeTab={activeTab}
+						onTabClick={setActiveTab}
+					/>
+					<ChatContent activeTab={activeTab} />
+				</ChatDialog>
+				<TanStackRouterDevtools />
+			</GamegooSocketProvider>
+		</TanstackQueryProvider>
 	);
 }
 
