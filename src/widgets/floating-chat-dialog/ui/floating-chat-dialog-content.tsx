2ce5c55f7f60850d975cd870fabee1086929210f
Sungwook Kim <silenc3@naver.com>
2025-10-14 00:13:03 +0900

    chore(#16): 훅 중복 호출 삭제

--- src/widgets/floating-chat-dialog/ui/floating-chat-dialog-content.tsx
+++ src/widgets/floating-chat-dialog/ui/floating-chat-dialog-content.tsx
@@ -1,10 +1,8 @@
 import { useChatDialogStore } from "@/entities/chat";
 import { Chatroom, ChatroomList } from "@/features/chat";
-import { useFriendOnline } from "@/features/friend";
 import FriendList from "@/features/friend/ui/friend-list";
 
 function FloatingChatDialogContent() {
-	useFriendOnline();
 	const { chatDialogType } = useChatDialogStore();
 
 	if (chatDialogType === "friend-list") {
