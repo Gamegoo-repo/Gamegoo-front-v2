3544f305745b6c85f0167acfeb82f53289bed3fc
조하은 <jhe93@naver.com>
2025-10-24 04:32:55 +0900

    feat(#31): useSignUpMutation 훅 구현

--- src/features/auth/api/auth-api.ts
+++ src/features/auth/api/auth-api.ts
@@ -1,7 +1,11 @@
 import { useMutation } from "@tanstack/react-query";
-import { AuthControllerApi } from "@/shared/api/@generated";
-import type { RefreshTokenRequest } from "@/shared/api/@generated";
-import { tokenManager } from "@/shared/api/config";
+import { useNavigate } from "@tanstack/react-router";
+import type {
+	RefreshTokenRequest,
+	RiotJoinRequest,
+} from "@/shared/api/@generated";
+import { AuthControllerApi, RiotApi } from "@/shared/api/@generated";
+import { apiConfiguration, tokenManager } from "@/shared/api/config";
 
 const authApi = new AuthControllerApi();
 
@@ -19,6 +23,44 @@ export const useRefreshToken = () => {
 		},
 		onError: (error) => {
 			console.warn("Token refresh failed:", error);
+		},
+	});
+};
+
+export const useSignUpMutation = () => {
+	const navigate = useNavigate();
+
+	return useMutation({
+		mutationFn: async (riotJoinRequestDto: RiotJoinRequest) => {
+			const api = new RiotApi(apiConfiguration);
+			const response = await api.joinByRSO(riotJoinRequestDto);
+			return response.data.data;
+		},
+		onSuccess: (data) => {
+			if (data) {
+				const { accessToken, refreshToken } = data;
+
+				if (accessToken && refreshToken) {
+					tokenManager.setTokens(accessToken, refreshToken);
+					navigate({
+						to: "/",
+					});
+				}
+			}
+		},
+		onError: (error) => {
+			console.warn("Token refresh failed:", error);
+			switch (error.name) {
+				case "MEMBER_403":
+					console.log("이미 존재하는 사용자입니다.");
+					return;
+				case "RIOT_402":
+					console.log("해당 Riot 계정이 존재하지 않습니다.");
+					return;
+				case "RIOT_401":
+					console.log("잘못된 Riot API 키입니다.");
+					return;
+			}
 		},
 	});
 };
--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,3 +1,4 @@
+export { useRefreshToken } from "./api/auth-api";
 export { type AuthCallbackParams, OAuthStatus } from "./api/dto";
 export { login } from "./api/login";
 export { LoginButton } from "./ui/login-button";
