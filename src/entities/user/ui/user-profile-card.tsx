409372012a5bd3f473ee2dedb5ead332b4d4832d
Sungwook Kim <silenc3@naver.com>
2025-11-12 15:17:25 +0900

    fix(#23): p태그안의 div태그 오류 수정

--- src/entities/user/ui/user-profile-card.tsx
+++ src/entities/user/ui/user-profile-card.tsx
@@ -11,15 +11,15 @@ export default function UserProfileCard({
 }) {
 	return (
 		<div className="flex gap-3 items-center">
-			<p className="relative">
+			<div className="relative">
 				<UserProfile id={profileImage} size={74} />
-			</p>
-			<p className="flex flex-col h-fit gap-1">
+			</div>
+			<div className="flex flex-col h-fit gap-1">
 				<span className="flex items-center justify-start gap-1.5 text-gray-800 bold-20 w-full">
 					{gameName}
 				</span>
 				<span className="text-gray-500 semibold-14">#{tag}</span>
-			</p>
+			</div>
 		</div>
 	);
 }
