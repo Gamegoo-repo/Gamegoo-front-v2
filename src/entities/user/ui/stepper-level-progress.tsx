041a53765d8b3e0db5a2198b20bc1a021d865def
조하은 <jhe93@naver.com>
2025-11-15 21:11:23 +0900

    refactor(#76): StepperLevelProgress 컴포넌트 코드 리팩토링

--- src/entities/user/ui/stepper-level-progress.tsx
+++ src/entities/user/ui/stepper-level-progress.tsx
@@ -1,8 +1,6 @@
-import type { ReactNode } from "react";
-import CheckIcon from "@/shared/assets/icons/ic-manner-check.svg?react";
 import { cn } from "@/shared/lib/utils";
-
-const STEPPER_COLORS = ["#C1B7FF", "#9F90F9", "#7B65FF", "#5A42EE", "#452AEA"]; //violet-300 ~ violet-700
+import { STEPPER_COLORS } from "../config/stepper-colors";
+import StepperStep from "./stepper-step";
 
 export default function StepperLevelProgress({
 	userLevel,
@@ -38,84 +36,16 @@ export default function StepperLevelProgress({
 
 			<ol className="progress-items w-full">
 				{LEVEL_NUMBERS.map((num) => {
-					// 유저 레벨보다 높은 노드들의 경우
-					if (num > userLevel) {
-						return <UnCheckedStepItem key={num} level={num} />;
-					} else if (num === userLevel) {
-						// 유저의 현재 레벨인 노드의 경우
-						return (
-							<div className="flex flex-col relative">
-								<CheckedStepItem key={num} level={num}>
-									<div
-										className="absolute bold-11 -top-14 -translate-y-[100%] text-nowrap flex flex-col items-center"
-										style={{
-											color:
-												num > 3 ? STEPPER_COLORS[num - 1] : STEPPER_COLORS[1],
-										}}
-									>
-										{rankPercentile && (
-											<>
-												<span>상위 {rankPercentile}%</span>
-												<div
-													className="w-0 h-0 border-x-4 border-x-transparent border-t-8"
-													style={{
-														borderTopColor:
-															num > 3
-																? STEPPER_COLORS[num - 1]
-																: STEPPER_COLORS[1],
-													}}
-												/>
-											</>
-										)}
-									</div>
-								</CheckedStepItem>
-							</div>
-						);
-					} else {
-						return <CheckedStepItem key={num} level={num} />;
-					}
+					return (
+						<StepperStep
+							key={num}
+							currentLevel={num}
+							userLevel={userLevel}
+							rankPercentile={rankPercentile}
+						/>
+					);
 				})}
 			</ol>
 		</div>
 	);
 }
-
-const CheckedStepItem = ({
-	level,
-	children,
-}: {
-	level: number;
-	children?: ReactNode;
-}) => {
-	return (
-		<li className="relative flex flex-col items-center justify-center">
-			<LevelText level={level} />
-			{children}
-			<div
-				className="w-10 h-10 flex items-center justify-center rounded-full"
-				style={{ backgroundColor: STEPPER_COLORS[level - 1] }}
-			>
-				<CheckIcon />
-			</div>
-		</li>
-	);
-};
-
-const UnCheckedStepItem = ({ level }: { level: number }) => {
-	return (
-		<li className="relative flex flex-col items-center justify-center">
-			<span className="inline-block absolute bold-14 -top-2 -translate-y-[100%] text-gray-800 text-nowrap">
-				Lv {level}
-			</span>
-			<div className="p-0.5 w-[20px] h-[20px] flex items-center justify-center rounded-full bg-gray-100 border-[5px] border-gray-800 shrink-0" />
-		</li>
-	);
-};
-
-const LevelText = ({ level }: { level: number }) => {
-	return (
-		<span className="absolute bold-14 -top-2 -translate-y-[100%] text-violet-400">
-			Lv {level}
-		</span>
-	);
-};
