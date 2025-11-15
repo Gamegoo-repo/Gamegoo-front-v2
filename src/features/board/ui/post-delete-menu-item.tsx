44d43a1db809019732f1e7f34e7b9369d414de51
조하은 <jhe93@naver.com>
2025-11-15 23:36:19 +0900

    feat(#72): 글 작성자(본인, 타인)에 따른 팝오버 렌더링 구현

---
+++ src/features/board/ui/post-delete-menu-item.tsx
@@ -0,0 +1,42 @@
+import { useMutation } from "@tanstack/react-query";
+import {
+	PopoverMenuItem,
+	type PopoverMenuItemProps,
+} from "@/features/popover-menu";
+import { api } from "@/shared/api";
+import { queryClient } from "@/shared/lib/query-client";
+import { boardKeys } from "../api/query-keys";
+
+interface PostDeleteMenuItemProps {
+	postId: number;
+	onClosePopover?: () => void;
+}
+
+export function PostDeleteMenuItem({
+	postId,
+	onClosePopover,
+}: PostDeleteMenuItemProps) {
+	const deletePostMutation = useMutation({
+		mutationFn: async () => {
+			const response = await api.private.board._delete(postId);
+			return response.data;
+		},
+		onSuccess: () => {
+			/** TODO: 쿼리 무효화 all로 해도 되는게 맞을지? */
+			queryClient.invalidateQueries({ queryKey: boardKeys.all });
+		},
+		onError: (error) => {},
+	});
+
+	const menuItemProps: PopoverMenuItemProps = {
+		text: "삭제하기",
+		onClick: () => {
+			deletePostMutation.mutate();
+			onClosePopover?.();
+			alert("게시물을 삭제했습니다.");
+		},
+		className: "text-red-600",
+	};
+
+	return <PopoverMenuItem {...menuItemProps} />;
+}
--- src/features/popover-menu/popover-menu.tsx
+++ src/features/popover-menu/popover-menu.tsx
@@ -1,7 +1,10 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useCallback, useContext, useMemo } from "react";
 import ThreeDotsButtonBlack from "@/shared/assets/icons/three_dots_button_black.svg?react";
 import { Button } from "@/shared/ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "@/shared/ui/popover";
+import { PopoverContext } from "@/shared/ui/popover/popover";
+import type { PostDeleteMenuItem } from "../board/ui/post-delete-menu-item";
+import type { PostEditMenuItem } from "../board/ui/post-edit-menu-item";
 import type {
 	BlockMenuItem,
 	ChatroomLeaveMenuItem,
@@ -9,33 +12,23 @@ import type {
 	FriendDeleteMenuItem,
 	ReportMenuItem,
 } from "./menu-items";
+import type { BlockToggleMenu } from "./menu-items/block-toggle-menu-item";
 
 type MenuItemComponent =
 	| typeof BlockMenuItem
+	| typeof BlockToggleMenu
 	| typeof ChatroomLeaveMenuItem
 	| typeof FriendAddMenuItem
 	| typeof FriendDeleteMenuItem
-	| typeof ReportMenuItem;
+	| typeof ReportMenuItem
+	| typeof PostDeleteMenuItem
+	| typeof PostEditMenuItem;
 
 interface PopoverMenuProps {
 	menuItems: React.ReactElement<MenuItemComponent>[];
 }
 
 function PopoverMenu({ menuItems }: PopoverMenuProps) {
-	const handleClose = () => {
-		console.log("닫힘");
-	};
-	const menuItemsWithCloseHandler = useMemo(() => {
-		return menuItems.map((menuItem) => {
-			return React.cloneElement(menuItem, {
-				...menuItem.props,
-				onClosePopover: handleClose,
-			} as React.ComponentProps<MenuItemComponent>);
-		});
-	}, [menuItems, handleClose]);
-
-	if (menuItems.length === 0) return null;
-
 	return (
 		<Popover>
 			<PopoverTrigger asChild>
@@ -48,17 +41,44 @@ function PopoverMenu({ menuItems }: PopoverMenuProps) {
 				</Button>
 			</PopoverTrigger>
 			<PopoverContent
-				side="right"
-				align="start"
+				// side="right"
+				// align="start"
 				showArrow={false}
 				className="w-48 p-0 bg-white rounded-[10px] shadow-lg"
 			>
-				<div className="[&>*:not(:last-child)]:border-b [&>*:not(:last-child)]:border-gray-200 text-gray-600 medium-14">
-					{menuItemsWithCloseHandler}
-				</div>
+				<PopoverMenuContent menuItems={menuItems} />
 			</PopoverContent>
 		</Popover>
 	);
 }
 
 export default PopoverMenu;
+
+function PopoverMenuContent({ menuItems }: PopoverMenuProps) {
+	const context = useContext(PopoverContext);
+
+	const handleClose = useCallback(() => {
+		context?.close();
+	}, [context]);
+
+	const menuItemsWithCloseHandler = useMemo(() => {
+		return menuItems.map((menuItem) => {
+			return React.cloneElement(menuItem, {
+				...menuItem.props,
+				onClosePopover: handleClose,
+			} as React.ComponentProps<MenuItemComponent>);
+		});
+	}, [menuItems, handleClose]);
+
+	if (menuItems.length === 0) return null;
+
+	return (
+		<div
+			// onClick={(e) => e.stopPropagation()}
+			className="[&>*:not(:last-child)]:border-b [&>*:not(:last-child)]:border-gray-200
+  text-gray-600 medium-14"
+		>
+			{menuItemsWithCloseHandler}
+		</div>
+	);
+}
