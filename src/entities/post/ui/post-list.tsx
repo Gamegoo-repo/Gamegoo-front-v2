abb4a15fd9c098b91391c671cff1b39d0f358bd7
조하은 <jhe93@naver.com>
2025-11-23 02:48:34 +0900

    feat(#83): 모바일 게시판 무한스크롤 구현

---
+++ src/entities/post/ui/post-list.tsx
@@ -0,0 +1,64 @@
+import { useFetchPostsWithCursorQuery } from "../model/use-mobile-post-list";
+import PostCard from "./post-card";
+import { useBoardFilterStore } from "@/features/board/model/board-filter-store";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import PostCardSkeletons from "./post-card-skeleton";
+
+export default function PostList() {
+	const { gameMode, tier, position, mike } = useBoardFilterStore();
+
+	const {
+		pages,
+		isLoading,
+		isError,
+		fetchNextPage,
+		isFetchingNextPage,
+		hasNextPage,
+	} = useFetchPostsWithCursorQuery({
+		gameMode: gameMode,
+		tier: tier,
+		mainP: position,
+		subP: undefined,
+		mike: mike,
+	});
+
+	const { ref, inView } = useInView();
+
+	useEffect(() => {
+		if (inView) {
+			fetchNextPage();
+		}
+	}, [inView, fetchNextPage]);
+
+	if (!pages || isLoading) {
+		return (
+			<div className="w-full px-5 pt-4 flex flex-col gap-4">
+				<PostCardSkeletons />
+			</div>
+		);
+	}
+
+	if (isError) {
+		return <></>;
+	}
+
+	return (
+		<div className="w-full px-5 pt-4 flex flex-col gap-4">
+			{pages.map((posts) => {
+				return (
+					<ul key={posts?.cursorId} className="flex flex-col gap-4">
+						{posts?.boards.map((post) => (
+							<li key={post.boardId}>
+								<PostCard {...post} />
+							</li>
+						))}
+					</ul>
+				);
+			})}
+			{pages && !isFetchingNextPage && hasNextPage && <div ref={ref} />}
+
+			{isFetchingNextPage && <PostCardSkeletons />}
+		</div>
+	);
+}
