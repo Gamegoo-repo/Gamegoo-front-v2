4ec141b737908ed2b04d5704ec4edbf3f0f4bb5d
조하은 <jhe93@naver.com>
2025-11-23 02:51:14 +0900

    feat(#83): 모바일 끌어올리기 버튼 구현

--- src/features/board/ui/bump-button.tsx
+++ src/features/board/ui/bump-button.tsx
@@ -10,7 +10,7 @@ export default function BumpButton({ className, ...props }: BumpButtonProps) {
 			className={`h-10 cursor-pointer flex items-center gap-1 hover:-translate-y-1 transition-transform duration-200 ease-out ${className || ""}`}
 			{...props}
 		>
-			<HoistingIcon />
+			<HoistingIcon className="text-violet-600" />
 			<span className="bold-14 bg-gradient-to-r from-violet-600 to-[#E02FC8] bg-clip-text text-transparent">
 				내가 쓴 글 끌어올리기
 			</span>
