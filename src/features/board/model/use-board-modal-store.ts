4050ba1203a19d938d006905a1e741be3a03a39d
조하은 <jhe93@naver.com>
2025-12-06 16:41:46 +0900

    feat(#58): board 페이지의 모달 상태 전역 스토어 작성

---
+++ src/features/board/model/use-board-modal-store.ts
@@ -0,0 +1,56 @@
+import { create } from "zustand";
+
+type ModalType = "detail" | "create" | "edit" | null;
+
+interface BoardModalStore {
+	activeModal: ModalType;
+	selectedPostId: number | null;
+
+	openDetailModal: (postId: number) => void;
+	openCreateModal: () => void;
+	// openEditModal: (postId: number, data: BoardFormData) => void;
+	openEditModal: (postId: number) => void;
+	closeModal: () => void;
+
+	isDetailOpen: () => boolean;
+	isCreateOpen: () => boolean;
+	isEditOpen: () => boolean;
+}
+
+export const useBoardModalStore = create<BoardModalStore>((set, get) => ({
+	activeModal: null,
+	selectedPostId: null,
+	postToEdit: null,
+
+	openDetailModal: (postId) => {
+		set({
+			activeModal: "detail",
+			selectedPostId: postId,
+		});
+	},
+
+	openCreateModal: () => {
+		set({
+			activeModal: "create",
+			selectedPostId: null,
+		});
+	},
+
+	openEditModal: (postId) => {
+		set({
+			activeModal: "edit",
+			selectedPostId: postId,
+		});
+	},
+
+	closeModal: () => {
+		set({
+			activeModal: null,
+			selectedPostId: null,
+		});
+	},
+
+	isDetailOpen: () => get().activeModal === "detail",
+	isCreateOpen: () => get().activeModal === "create",
+	isEditOpen: () => get().activeModal === "edit",
+}));
