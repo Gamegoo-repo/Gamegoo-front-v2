30be80663e9ecd8df05b7d92cc9b5dff69663d47
조하은 <jhe93@naver.com>
2025-12-06 16:09:57 +0900

    fix(#95): 끌어올리기 버튼  모바일에서 안보이는 문제 해결

--- src/features/board/ui/bump-button.tsx
+++ src/features/board/ui/bump-button.tsx
@@ -16,15 +16,15 @@ export default function BumpButton({ className, ...props }: BumpButtonProps) {
 				"flex cursor-pointer items-center justify-center",
 				"transition-transform duration-200 ease-out",
 				"mobile:hover:-translate-y-1 mobile:h-10 mobile:gap-1",
-				"h-full rounded-md bg-linear-to-tr from-violet-600 to-[#E02FC8] px-3 py-2 text-white",
-				"mobile:h-fit bg-none p-0",
+				"aspect-square h-full rounded-md bg-linear-to-tr from-violet-600 to-[#E02FC8] px-3 py-2 text-white",
+				"mobile:h-fit mobile:w-fit mobile:bg-none p-0",
 				className,
 			)}
 			onClick={() => mutate()}
 			{...props}
 		>
 			<HoistingIcon className="h-3.5 mobile:h-3 mobile:w-3 w-3.5 mobile:text-violet-600 text-white" />
-			<span className="bold-14 mobile:block hidden bg-gradient-to-r from-violet-600 to-[#E02FC8] bg-clip-text text-transparent">
+			<span className="bold-14 mobile:block hidden bg-gradient-to-r from-violet-600 to-[#E02FC8] mobile:bg-clip-text mobile:text-transparent">
 				내가 쓴 글 끌어올리기
 			</span>
 		</button>
