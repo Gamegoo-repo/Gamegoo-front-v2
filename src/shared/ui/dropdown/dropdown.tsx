eebf2f7d38edb48c0224c30dce893a64475fc04c
조하은 <jhe93@naver.com>
2025-12-12 19:13:55 +0900

    fix(#92): 드롭다운 코드 수정

--- src/shared/ui/dropdown/dropdown.tsx
+++ src/shared/ui/dropdown/dropdown.tsx
@@ -6,61 +6,48 @@ import { cn } from "@/shared/lib/utils";
 import { useClickOutside } from "@/shared/model/useClickOutside";
 
 const dropdownVariants = cva(
-	"flex items-center justify-between border-1 border-gray-300 bg-white text-gray-800 w-full",
+	"flex items-center justify-between w-full border border-gray-300 bg-white text-gray-800 h-full mobile:rounded-[10px] rounded-md mobile:px-4 mobile:py-3 py-2 pr-2 pl-3 font-normal mobile:font-medium mobile:text-base text-[13px]",
 	{
 		variants: {
 			variant: {
 				primary: "",
 				secondary: "",
 			},
-			size: {
-				sm: "px-2 py-1.5 text-xs h-8",
-				md: "rounded-md px-2 pl-3 pr-2 text-[13px] h-10 md:p-4 md:text-base md:rounded-[10px] font-normal text-[13px] h-9", // mobile
-				lg: "px-4 py-3 text-base rounded-[10px] font-medium text-base h-15", // desktop
-			},
 		},
 		defaultVariants: {
 			variant: "primary",
-			size: "md",
 		},
 	},
 );
 
-const dropdownMenuVariants = cva("w-full h-auto shadow-md rounded-md", {
-	variants: {
-		variant: {
-			primary: "bg-gray-200",
-			secondary: "bg-white",
-		},
-		size: {
-			sm: "rounded-md",
-			md: "rounded-md md:rounded-[10px]",
-			lg: "rounded-lg",
-		},
-	},
-	defaultVariants: {
-		variant: "primary",
-		size: "md",
-	},
-});
-
-const dropdownItemVariants = cva(
-	"relative flex items-center w-full leading-normal first:rounded-t-md last:rounded-b-md",
+const dropdownMenuVariants = cva(
+	"w-full h-auto shadow-md rounded-md mobile:rounded-[10px] transition-colors duration-200 ",
 	{
 		variants: {
 			variant: {
-				primary: "hover:text-white hover:bg-violet-600",
-				secondary: "hover:text-violet-600 hover:bg-gray-100",
-			},
-			size: {
-				sm: "py-1.5 px-2 text-xs",
-				md: "py-2.5 px-4 text-[13px] font-normal md:text-base md:font-medium",
-				lg: "py-3 px-5 text-base",
+				primary: "bg-gray-200",
+				secondary: "bg-white",
 			},
 		},
 		defaultVariants: {
 			variant: "primary",
-			size: "md",
+		},
+	},
+);
+
+const dropdownItemVariants = cva(
+	"relative flex w-full items-center mobile:px-4 px-3 mobile:py-3 py-2 mobile:font-medium mobile:text-base text-[13px] text-gray-800 leading-normal first:rounded-t-md last:rounded-b-md",
+	{
+		variants: {
+			variant: {
+				primary:
+					"hover:text-white hover:bg-violet-600 active:text-white active:bg-violet-600",
+				secondary:
+					"hover:text-violet-600 hover:bg-gray-100 active:text-violet-600 active:bg-gray-100",
+			},
+		},
+		defaultVariants: {
+			variant: "primary",
 		},
 	},
 );
@@ -75,9 +62,10 @@ interface DropdownProps<T>
 	extends Omit<React.HTMLAttributes<HTMLDivElement>, "onSelect">,
 		VariantProps<typeof dropdownVariants> {
 	selectedLabel: string;
-	size: "sm" | "md" | "lg";
 	items: DropdownItem<T>[];
 	onSelect: (id: T) => void;
+	width?: string | number;
+	height?: string | number;
 }
 
 export default function Dropdown<T>({
@@ -85,7 +73,6 @@ export default function Dropdown<T>({
 	items,
 	onSelect,
 	variant = "primary",
-	size = "md",
 	className,
 	...restProps
 }: DropdownProps<T>) {
@@ -105,12 +92,6 @@ export default function Dropdown<T>({
 		setOpen(false);
 	};
 
-	const iconSizeClass = {
-		sm: "w-2",
-		md: "w-3",
-		lg: "w-4",
-	}[size];
-
 	return (
 		<div ref={menuRef} className={cn("relative", className)} {...restProps}>
 			<button
@@ -119,13 +100,13 @@ export default function Dropdown<T>({
 				aria-expanded={open}
 				aria-controls="dropdown-menu"
 				type="button"
-				className={dropdownVariants({ variant, size })}
+				className={dropdownVariants({ variant })}
 				onClick={handleToggle}
 			>
 				{selectedLabel}
 				<ArrowIcon
 					className={cn(
-						iconSizeClass,
+						"mobile:w-4 w-3",
 						"transition-all duration-300 ease-in-out",
 						open && "rotate-180",
 					)}
@@ -133,17 +114,14 @@ export default function Dropdown<T>({
 			</button>
 			{open && (
 				<div className="absolute top-full left-0 z-10 w-full">
-					<div
-						role="listbox"
-						className={dropdownMenuVariants({ variant, size })}
-					>
+					<div role="listbox" className={dropdownMenuVariants({ variant })}>
 						{items.map((item) => (
 							<button
 								type="button"
 								role="option"
 								key={item.title}
 								onClick={() => handleItemClick(item.id)}
-								className={dropdownItemVariants({ variant, size })}
+								className={dropdownItemVariants({ variant })}
 							>
 								{item.url ? (
 									<Link
