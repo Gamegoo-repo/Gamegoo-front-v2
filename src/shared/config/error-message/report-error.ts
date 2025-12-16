55aa0aa85a6f2b6fb8c3841e47d959fad21a8603
조하은 <jhe93@naver.com>
2025-12-16 17:53:45 +0900

    feat(#83): REPORT_ERROR 타입 가드 추가

--- src/shared/config/error-message/report-error.ts
+++ src/shared/config/error-message/report-error.ts
@@ -1,7 +1,13 @@
 export const REPORT_ERROR_MESSAGES = {
 	REPORT_402: "해당 신고 건을 찾을 수 없습니다",
 	REPORT_403:
-		"신고 접수 경로 정보를 찾을 수 없습니다. 관리자에게 문의 바랍니다",
+		"신고 접수 경로 정보를 찾을 수 없습니다.\n관리자에게 문의 바랍니다",
 	REPORT_404:
-		"해당 회원에 대한 신고가 이미 등록되었습니다. 내일 다시 시도해주세요",
+		"해당 회원에 대한 신고가 이미 등록되었습니다.\n내일 다시 시도해주세요.",
 } as const;
+
+export const isReportErrorCode = (
+	code: string,
+): code is keyof typeof REPORT_ERROR_MESSAGES => {
+	return code in REPORT_ERROR_MESSAGES;
+};
