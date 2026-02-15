ccf234afa4dd9c8757b196295902e785674a9ae8
kwangyoulsagong <sgky0511@naver.com>
2026-02-15 21:04:36 +0900

    chore: update responsive floating card

--- src/widgets/match/ui/floating-matching-card/floating-matching-card.tsx
+++ src/widgets/match/ui/floating-matching-card/floating-matching-card.tsx
@@ -30,15 +30,20 @@ export const FloatingMatchingCard = () => {
 		matchFlow.cancel(sessionId);
 		setStep("profile");
 	};
+
 	return (
-		<section className="fixed w-[328px] h-[185px] left-6 bottom-20 z-50 rounded-[20px] border border-gray-300 bg-gray-100 animate-slide-in">
-			<header className="absolute flex justify-between p-3 w-full z-1">
-				<Button variant="ghost" className="py-3" onClick={handleCancelMatching}>
+		<section className="fixed  w-[191px] h-[108px] md:w-[328px] md:h-[185px] left-6 bottom-6 z-50 rounded-[11px] md:rounded-[20px] border border-gray-300 bg-gray-100 animate-slide-in">
+			<header className="absolute flex justify-between p-[6px] md:p-3 w-full z-1">
+				<Button
+					variant="ghost"
+					className="p-[6px] [&:has(>svg)]:px-[6px] md:p-3 md:[&:has(>svg)]:px-3 "
+					onClick={handleCancelMatching}
+				>
 					<CancelMatchingIcon />
 				</Button>
 				<Button
 					variant="ghost"
-					className="py-3"
+					className="p-[6px] [&:has(>svg)]:px-[6px] md:p-3 md:[&:has(>svg)]:px-3 "
 					onClick={handleExpandMatchingPage}
 				>
 					<ExpandMatchingIcon />
@@ -46,11 +51,11 @@ export const FloatingMatchingCard = () => {
 			</header>
 			<div className="flex animate-fade-in py-3 flex-col items-center justify-center gap-2 h-full">
 				<div className="animate-grow-shrink">
-					<HeartIcon className="w-[92px] h-[92px]" />
+					<HeartIcon className="w-[54px] h-[54px] md:w-[92px] md:h-[92px]" />
 				</div>
-				<div className="flex flex-col items-center">
+				<div className="flex flex-col items-center w-full">
 					{/* 랜덤 메세지 */}
-					<span className="text-gray-800 regular-11">
+					<span className="text-gray-800 text-[8px] md:text-[11px] ">
 						어떤 사람이 나올까요?
 					</span>
 					{/* 시간 표시 */}
